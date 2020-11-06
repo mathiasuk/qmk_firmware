1dfa086e8edc1c801c7faddab66f26de6f60f571
Mathias André <mathias@acronycal.org>
2020-11-06 22:11:58 +0100

    Misc updates.

--- keyboards/lily58l/keymaps/0xdeadbee/rules.mk
+++ keyboards/lily58l/keymaps/0xdeadbee/rules.mk
@@ -2,6 +2,8 @@ OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # ENables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB light
 
+WPM_ENABLE = yes
+
 # uncomment to the line to customize the setting
 #define ENCODER_DIRECTION_FLIP # Switch encoder’s directions
 #define ENCODER_RESOLUTION 4 # Tune encoder resolution
