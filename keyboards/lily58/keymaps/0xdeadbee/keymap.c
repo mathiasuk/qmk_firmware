336595782a3948c7ac8d9930479b017b4c270c64
Mathias André <818222+mathiasuk@users.noreply.github.com>
2023-10-18 10:52:20 +0200

    Add mod keys to home row.

--- keyboards/lily58/keymaps/0xdeadbee/keymap.c
+++ keyboards/lily58/keymaps/0xdeadbee/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------| Mute  |    |BackSP |------+------+------+------+------+------|
+ * |------+------+------+------+------+------| Mute  |    | RAlt  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LGUI | LAlt |LOWER | /Enter  /       \Space \  |RAISE |BackSP| RGUI |
@@ -52,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT( \
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-  MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MT(MOD_RCTL, KC_QUOT), \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,  KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+  MT(MOD_LCTL, KC_ESC), MT(MOD_LCTL, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LGUI, KC_F), KC_G,                    KC_H,    MT(MOD_RGUI, KC_J),    MT(MOD_RSFT, KC_K),    MT(MOD_RALT, KC_L),    MT(MOD_RCTL, KC_SCLN), MT(MOD_RCTL, KC_QUOT), \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,  KC_RALT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
                              KC_LGUI, KC_LALT, MO(_LOWER), KC_ENT,  KC_SPC, MO(_RAISE),  KC_BSPC, KC_RGUI \
 ),
 /* GAME
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_GAME] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
-  KC_ESC, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    _______,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
                              _______, _______, _______, _______, _______,  _______, _______, _______ \
                              /*_______, _______, _______, KC_SPC, KC_ENT,  _______, _______, _______ \*/
