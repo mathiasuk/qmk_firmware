76a0865f37f28e0b681670c71f16195759e6d560
Mathias André <818222+mathiasuk@users.noreply.github.com>
2024-04-22 14:49:46 +0200

    misc

--- keyboards/lily58/keymaps/0xdeadbee/keymap.c
+++ keyboards/lily58/keymaps/0xdeadbee/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------| Mute  |    | RAlt  |------+------+------+------+------+------|
+ * |------+------+------+------+------+------| Mute  |    |   ]   |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LGUI | LAlt |LOWER | /Enter  /       \Space \  |RAISE |BackSP| RGUI |
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   MT(MOD_LCTL, KC_ESC), KC_A, MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), KC_G,                    KC_H,    MT(MOD_RCTL, KC_J),    MT(MOD_RSFT, KC_K),    MT(MOD_RALT, KC_L),    KC_SCLN, MT(MOD_RCTL, KC_QUOT), \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,  KC_F13,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
                              KC_LGUI, KC_LALT, MO(_LOWER), KC_ENT,  KC_SPC, MO(_RAISE),  KC_BSPC, KC_RGUI \
 ),
 /* GAME
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      | F11  | F12  |      |      |      |                    |      |      |      |   [  |   ]  |   \  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   !  |   @  |   #  |   $  |   %  |-------.    ,-------| Left | Down |  Up  |Right |      |      |
- * |------+------+------+------+------+------| A+S+M |    |Delete |------+------+------+------+------+------|
+ * |------+------+------+------+------+------| A+S+M |    |   [   |------+------+------+------+------+------|
  * |      |      |      |CTL+C |CTL+V |      |-------|    |-------|      |   _  |   +  |      |      |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /       /       \      \  |RAISE |Delete| RGUI |
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_EQL, \
   _______, KC_F11,  KC_F12,  _______, _______, _______,                   _______, _______, _______, KC_LBRC,  KC_RBRC, KC_BSLS,\
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______, _______, \
-  _______, _______, _______, C(KC_C), C(KC_V), _______, LSA(KC_M), KC_DEL,  XXXXXXX, KC_UNDS, KC_PLUS, _______,  _______, KC_PIPE, \
+  _______, _______, _______, C(KC_C), C(KC_V), _______, LSA(KC_M), KC_LBRC,  XXXXXXX, KC_UNDS, KC_PLUS, _______,  _______, KC_PIPE, \
                              _______, _______, _______, _______, _______, _______, KC_DEL,  _______\
 ),
 /* RAISE
