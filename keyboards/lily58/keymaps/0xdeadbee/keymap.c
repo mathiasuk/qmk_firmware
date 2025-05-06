997637b8344f2ff552c7ba22bf7a840343e6828f
Mathias André <818222+mathiasuk@users.noreply.github.com>
2025-05-06 22:06:35 +0200

    Add GUI to S/L

--- keyboards/lily58/keymaps/0xdeadbee/keymap.c
+++ keyboards/lily58/keymaps/0xdeadbee/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT( \
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-  MT(MOD_LCTL, KC_ESC), KC_A, MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), KC_G,                    KC_H,    MT(MOD_RCTL, KC_J),    MT(MOD_RSFT, KC_K),    MT(MOD_RALT, KC_L),    KC_SCLN, MT(MOD_RCTL, KC_QUOT), \
+  MT(MOD_LCTL, KC_ESC), KC_A, MT(MOD_LGUI, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), KC_G,                    KC_H,    MT(MOD_RCTL, KC_J),    MT(MOD_RSFT, KC_K),    MT(MOD_LGUI, KC_L),    KC_SCLN, MT(MOD_RCTL, KC_QUOT), \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,  KC_F13,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
                              KC_LGUI, KC_LALT, MO(_LOWER), KC_ENT,  KC_SPC, MO(_RAISE),  KC_BSPC, KC_RGUI \
 ),
